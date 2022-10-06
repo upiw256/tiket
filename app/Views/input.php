139cb9fc28085ba6d4d0f35efc6c70ce4234985f
upiw256 <bilqimlb@gmail.com>
2022-10-06 11:32:12 +0700

    form simpan

--- app/Views/input.php
+++ app/Views/input.php
@@ -48,7 +48,37 @@
 	</nav>
 
 	<!-- Form -->
-
+    <div class="w-full max-w-xs m-10">
+  <form action="/simpan" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="nama">
+        Nama
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nama" type="text" placeholder="nama">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        NIS
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="nis" type="number" placeholder="nis">
+      
+    </div>
+    <div class="mb-6">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Jenis
+      </label>
+    <select name="jenis"class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+        <option value="siswa">Siswa</option>
+        <option value="umum">Umum</option>
+    </select>
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Simpan
+      </button>
+    </div>
+  </form>
+</div>
 
 	</body>
 </html>
