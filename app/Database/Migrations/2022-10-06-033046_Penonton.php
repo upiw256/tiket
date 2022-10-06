66b356d6a6f539e71bc48b60e283eea49120a2e8
upiw256 <bilqimlb@gmail.com>
2022-10-06 11:08:43 +0700

    Setting

---
+++ app/Database/Migrations/2022-10-06-033046_Penonton.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Penonton extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField(array(
+			'id_penonton' => array(
+					'type' => 'INT',
+					'constraint' => 5,
+					'unsigned' => TRUE,
+					'auto_increment' => TRUE
+			),
+			'nis' => array(
+					'type' => 'VARCHAR',
+					'constraint' => '100',
+			),
+			'nama' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+			),
+			'jenis' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+			),
+		));
+		$this->forge->addKey('id_penonton', TRUE);
+		$this->forge->createTable('penonton');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('penonton');
+	}
+}
